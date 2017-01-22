8243b94c06176e7f61be9a12dae21a50b95e57a7
ThibaultDBVLL <thibault.daboville@gmail.com>
2017-01-22 14:28:05 +0100

    initial commit

---
+++ diapo sans poo/js/json.js
@@ -0,0 +1,5 @@
+({"picture1" : "http://crevette63410.c.r.pic.centerblog.net/dogg4il3.png",
+"picture2" : "https://assets-cdn.github.com/images/modules/logos_page/Octocat.png",
+"picture3" : "http://xaharts.org/funny/i/gitopuss/github-octocat_yes-we-code.jpg"
+
+})inner.html
